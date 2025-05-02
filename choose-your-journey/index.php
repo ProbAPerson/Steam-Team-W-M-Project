16fc6e507349bcae743882b9d3dea0a03f5e8944
DeadPixelPro <99194146+DeadPixelPro@users.noreply.github.com>
2025-05-02 19:52:54 -0400

    Choose Your Journey

---
+++ choose-your-journey/index.php
@@ -0,0 +1,11 @@
+<?php
+$path = "../";
+$page = "Choose your Journey";
+include($path . "inc/header.php");
+?>
+
+<!--This will have links to all paths, including one "I don't care" path that will go to Topic 1-->
+
+<?php
+include($path . "inc/footer.php");
+?>
