7163f3c675df388b4a0aeb87d1a025a23951b521
DeadPixelPro <99194146+DeadPixelPro@users.noreply.github.com>
2025-05-02 21:38:23 -0400

    Update choose-your-journey

--- choose-your-journey/index.php
+++ choose-your-journey/index.php
@@ -3,9 +3,41 @@ $path = "../";
 $page = "Choose your Journey";
 include($path . "inc/header.php");
 ?>
-
-<!--This will have links to all paths, including one "I don't care" path that will go to Topic 1-->
-
+<div class="tutorial">
+    <h1>Choose your UNIX journey</h1>
+    <p>
+        These guides have follow-along examples for those new to UNIX.
+        There are several ways you can get started with your UNIX journey.
+        Alternatively, you may wish to skip to the first tutorial.
+    </p>
+    <div class="choose">
+        <a href="linux/">
+            <div class="chooseOption">
+                Linux
+            </div>
+        </a>
+        <a href="ssh/">
+            <div class="chooseOption">
+                SSH
+            </div>
+        </a>
+        <a href="macos/">
+            <div class="chooseOption">
+                macOS Terminal
+            </div>
+        </a>
+        <a href="wsl/">
+            <div class="chooseOption">
+                WSL
+            </div>
+        </a>
+        <a href="[TEMP]-TUTORIAL-1">
+            <div class="chooseOption">
+                I don't care, let's get on with it!
+            </div>
+        </a>
+    </div>
+</div>
 <?php
 include($path . "inc/footer.php");
 ?>
