78a9e37b0c87e984647a72d059cea7adc00a1de1
DeadPixelPro <99194146+DeadPixelPro@users.noreply.github.com>
2025-05-02 21:40:15 -0400

    Linux moment

---
+++ choose-your-journey/linux/index.php
@@ -0,0 +1,55 @@
+<?php
+$path = ".../";
+$page = "Getting Started - Linux";
+include($path . "inc/header.php");
+?>
+<div class="tutorial">
+    <h1>Linux</h1>
+    <p>
+        There are several major distributions of Linux, each with different focuses and benefits. 
+    </p>
+    <h2>Ubuntu</h2>
+    <p>
+        Ubuntu is one of the most popular Linux distributions, with a focus on user friendliness.
+        It’s easy to install, simple to use, and has lots of support documentation.
+        It uses the GNOME desktop environment.
+        Ubuntu’s installer with a built-in guide can be found <a href="https://ubuntu.com/desktop" target="_blank">here</a>.
+    </p>
+    <h2>Debian</h2>
+    <p>
+        Debian is regarded as a stability-first OS, and is very customizable.
+        It’s used as the base of many other different linux distros.
+        You can choose your desktop environment with Debian.
+        As part of the stability-first approach, releases are much less frequent.
+        Debian’s installer with a built-in install guide can be found <a href="https://www.debian.org/" target="_blank">here</a>.
+    </p>
+    <h2>Linux Mint</h2>
+    <p>
+        Linux Mint is built off of Ubuntu, but has a more polished interface, perfect for people with experience using Windows.
+        It uses the Cinnamon Desktop.
+        You can also get Linux Mint Debian Edition, which is based off of Debian instead.
+        Mint’s install guide can be found <a href="https://linuxmint-installation-guide.readthedocs.io/en/latest/" target="_blank">here</a>.
+    </p>
+    <h2>Pop!_OS</h2>
+    <p>
+        Pop!_OS is a gaming-centered distro with focus on support for high-end gaming graphics cards and an optimized version of the GNOME desktop.
+        It’s preferred by gamers, but also used by developers and power users.
+        More information can be found <a href="https://pop.system76.com/" target="_blank">here</a>.
+    </p>
+    <h2>Fedora Linux</h2>
+    <p>
+        Fedora is a distro backed by Red Hat, and is primarily an enterprise-focused distro.
+        It focuses on newer technologies for developers.
+        It uses Wayland as its desktop.
+        More information about Fedora’s workstation OS can be found <a href="https://fedoraproject.org/workstation/" target="_blank">here</a>.
+    </p>
+    <h2>Arch</h2>
+    <p>
+        Arch is a distro generally regarded as being for advanced Linux users who want full and total control of their machine.
+        As a new install, Arch only comes with the basics, requiring the user to install their window manager or desktop environment of their choice.
+        Arch’s install guide can be found <a href="https://wiki.archlinux.org/title/Installation_guide" target="_blank">here</a>.
+    </p>
+</div>
+<?php
+include($path . "inc/footer.php");
+?>
