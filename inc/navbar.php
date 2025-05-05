38835562b4e69475b88df908957cfcb0aa40b426
ProbAPerson <ccyanus7@gmail.com>
2025-05-04 23:21:52 -0400

    getting rid of upper blank space

--- inc/navbar.php
+++ inc/navbar.php
@@ -33,7 +33,6 @@ $currentPageName = $pageNames[$currentPage] ?? 'Page';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Gothic+A1&family=Oswald:wght@200..700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
->
     <!--End Google moment-->
 </head>
 <header>
